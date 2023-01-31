39db7215860351d80d428a95a8d1a8ddee82b08d
zachlamont <zachlamont1@gmail.com>
2023-01-31 16:35:58 +1000

    Home page complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,59 @@
 console.log("Zach's Restaurant page");
+
+function displayHome() {
+  const content = document.getElementById("content");
+
+  const backgroundImage = document.createElement("div");
+  backgroundImage.classList.add("background-image");
+  content.appendChild(backgroundImage);
+
+  const header = document.createElement("header");
+  const nav = document.createElement("nav");
+  const ul = document.createElement("ul");
+  const menuItem = document.createElement("li");
+  const menuLink = document.createElement("a");
+  menuLink.href = "./menu.html";
+  menuLink.textContent = "MENU";
+  menuItem.appendChild(menuLink);
+  ul.appendChild(menuItem);
+  const aboutItem = document.createElement("li");
+  const aboutLink = document.createElement("a");
+  aboutLink.href = "#";
+  aboutLink.textContent = "ABOUT";
+  aboutItem.appendChild(aboutLink);
+  ul.appendChild(aboutItem);
+  const contactItem = document.createElement("li");
+  const contactLink = document.createElement("a");
+  contactLink.href = "#";
+  contactLink.textContent = "CONTACT";
+  contactItem.appendChild(contactLink);
+  ul.appendChild(contactItem);
+  nav.appendChild(ul);
+  header.appendChild(nav);
+  content.appendChild(header);
+
+  const main = document.createElement("main");
+  main.id = "scroll-container";
+  const logo = document.createElement("div");
+  logo.classList.add("logo");
+  const cafe = document.createElement("div");
+  cafe.classList.add("cafe");
+  cafe.textContent = "Café";
+  logo.appendChild(cafe);
+  const fofinho = document.createElement("div");
+  fofinho.classList.add("fofinho");
+  fofinho.textContent = "Fofinho";
+  logo.appendChild(fofinho);
+  main.appendChild(logo);
+  content.appendChild(main);
+
+  const footer = document.createElement("footer");
+  const p = document.createElement("p");
+  p.textContent = "Developed with ♡ by Zach Lamont";
+  footer.appendChild(p);
+  content.appendChild(footer);
+}
+
+if (window.location.pathname === '/dist/index.html') {
+    window.onload = displayHome;
+  }
